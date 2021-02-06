eaf836f0f7d1400a350a3b35ca97592d3cfdc9a2
vladimirKartyshov <vovan198080"inbox.ru>
2021-02-06 19:37:26 +0300

    axios

--- src/plugins/axios/index.js
+++ src/plugins/axios/index.js
@@ -1,12 +1,11 @@
 import axios from 'axios';
+import interceptors from "@/plugins/axios/interceptors";
 
 const instance = axios.create({
     baseURL: process.env.VUE_APP_API_URL,
-    // params: {
-    //     apikey: process.env.VUE_APP_API_KEY,
-    //     plot: "full"
-    // }
 });
+
+interceptors(instance);
 
 export default instance;
 
